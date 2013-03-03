c8fc7fa5d213269cd2d3ec72ad0f5cb5c9a6a415
slightair <arksutite@gmail.com>
2013-03-03 14:09:54 +0900

    タイトル画面っぽくした

--- momoKinokoGame/MKIntroLayer.m
+++ momoKinokoGame/MKIntroLayer.m
@@ -9,6 +9,15 @@
 #import "MKIntroLayer.h"
 #import "MKGameEngine.h"
 
+#define kLabelFontName @"HiraKakuProN-W6"
+#define kLabelFontSize 24
+
+@interface MKIntroLayer ()
+
+- (void)addTitleLabel:(NSString *)title position:(CGPoint)position;
+
+@end
+
 @implementation MKIntroLayer
 
 + (CCScene *)scene
@@ -26,10 +35,14 @@
     [super onEnter];
 
     CGSize windowSize = [[CCDirector sharedDirector] winSize];
+
     CCSprite *background = [CCSprite spriteWithFile:@"background.png"];
     background.position = ccp(windowSize.width / 2, windowSize.height / 2);
-
     [self addChild:background];
+
+    [self addTitleLabel:@"エクストリーム" position:ccp(windowSize.width / 2, windowSize.height / 2 + 100)];
+    [self addTitleLabel:@"もも・きのこ狩り" position:ccp(windowSize.width / 2, windowSize.height / 2 + 60)];
+    [self addTitleLabel:@"はじめる" position:ccp(windowSize.width / 2, windowSize.height / 2 - 80)];
 
     [[[CCDirector sharedDirector] touchDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
 }
@@ -49,6 +62,20 @@
 - (void)ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event
 {
     [[MKGameEngine sharedEngine] startNewGame];
+}
+
+- (void)addTitleLabel:(NSString *)title position:(CGPoint)position
+{
+    CGSize shadowOffset = CGSizeMake(1, -1);
+
+    CCLabelTTF *shadowLabel = [CCLabelTTF labelWithString:title fontName:kLabelFontName fontSize:kLabelFontSize];
+    shadowLabel.position = ccp(position.x + shadowOffset.width, position.y + shadowOffset.height);
+    shadowLabel.color = ccc3(0, 0, 0);
+    [self addChild:shadowLabel];
+
+    CCLabelTTF *label = [CCLabelTTF labelWithString:title fontName:kLabelFontName fontSize:kLabelFontSize];
+    label.position = position;
+    [self addChild:label];
 }
 
 @end
